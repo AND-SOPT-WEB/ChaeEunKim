e24632a5f197a8c79dde3e5727cdfb1e295f0f6e
bongtta <kce0419@naver.com>
2024-11-02 16:00:59 +0900

    feat: 페이지 구성

---
+++ week3/hw3/1to50game/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='game'>
+      <header className='header'>
+        <h1>1 to 50</h1>
+        <button className='game-btn'>게임</button>
+        <button className='rank-btn'>랭킹</button>
+      </header>
+    </div>
+  )
+}
+
+export default App
